0dfb7e1540c159c59ff3623e01adcbd3ff3027a7
unknown <jadentr.44@gmail.com>
2022-06-28 22:09:31 -0500

    woking timer

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,27 @@
 // global varaibales
 let questions = []
 
+let timerDisplay = document.querySelector("#timer")
+
 
 //queryselectors
-document.querySelector("#button").addEventListener("click", playGame)
+document.querySelector("#startButton").addEventListener("click", playGame)
 
 
 //functions
 function playGame(){
-  console.log("button")
+  let secondsLeft = 5;
+  displayGame()
+  let timer = setInterval(function(){
+    secondsLeft--;
+    timerDisplay.textContent = secondsLeft
+    console.log(secondsLeft)
+    if(secondsLeft == 0){
+      clearInterval(timer)
+      stopGame()
+    }
+  },1000)
 }
+
+function displayGame(){}
+function stopGame(){}
