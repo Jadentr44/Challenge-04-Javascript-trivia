fda8c8e929a0271b544b43ece637c03c96f9a559
unknown <jadentr.44@gmail.com>
2022-06-28 21:45:40 -0500

    button starts game

---
+++ assets/js/game.js
@@ -0,0 +1,12 @@
+// global varaibales
+let questions = []
+
+
+//queryselectors
+document.querySelector("#button").addEventListener("click", playGame)
+
+
+//functions
+function playGame(){
+  console.log("button")
+}
